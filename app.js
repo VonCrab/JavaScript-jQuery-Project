1f09f0ef2ae8a516c15e06ad41c73d5d2b6a3a1b
Conservacrab <lukepensiero@gmail.com>
2015-06-03 16:54:08 -0400

    Second commit, updated app.js and stylesheet.css

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
+var x;
+
 var main = function() {
 
   $( 'body' ).append( "<div id='overlord'></div>");
 
-  for ( var x = 0 ; x < 4 ; x++ ) {
-    $( '#overlord' ).append( "<div class='x-row'></div>" );
-  }
+  drawGrid(256);
 
-  for ( var y = 0 ; y < 4 ; y++ ) {
-    $( '.x-row' ).append( "<div class='grid-square'></div>" );
-  }
+  $( '.grid-square' )
+    .mouseenter( function() {
+      $( '.active' ).addClass( 'trail' );
+      $( '.active' ).removeClass( 'active' );
 
-  $('.grid-square')
-    .mouseenter(function() {
-      $('.active').addClass('trail');
-      $('.active').removeClass('active');
-
-      $(this).addClass('active');
+      $(this).addClass( 'active' );
   });
+
+};
+
+var drawGrid = function ( tiles ) {
+  x = 0;
+
+  var tilesPerRow = Math.sqrt( tiles );
+
+  for ( ; x < tiles ; x++ ) {
+    $( '#overlord' ).append( "<div class='grid-square'></div>" );
+  }
+
+  $('.grid-square').width ( (960 -  tilesPerRow * 2)  / tilesPerRow );
+  $('.grid-square').height ( (960 - tilesPerRow * 2) / tilesPerRow );
 
 };
 
