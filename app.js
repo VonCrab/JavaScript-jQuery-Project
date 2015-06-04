4d0f7747c8ec6651773a31bfc8817882a9b1685d
Conservacrab <lukepensiero@gmail.com>
2015-06-04 09:58:52 -0400

    Added buttons to draw and clear trail, minor CSS tweeks, improving js

--- app.js
+++ app.js
@@ -1,10 +1,46 @@
-var x;
+var x,
+  squareHeightWidth;
 
 var main = function() {
 
-  $( 'body' ).append( "<div id='overlord'></div>");
+  //Initialization of webpage
+  $( '.container' ).append( "<div id='overlord'></div>");
+  drawGrid(16);
+  mouseTrailMode();
 
-  drawGrid(256);
+
+  $( '#build-grid' )
+    .click( function() {
+      gridSize = prompt( "Enter the length of the grid:" );
+      drawGrid( gridSize );
+    });
+
+    $( '#clear-grid' )
+      .click( function() {
+        $( '.active' ).removeClass('active');
+        $( '.trail' ).removeClass('trail')
+      });
+
+};
+
+var drawGrid = function ( rows ) {
+  x = 0,
+  squareHeightWidth =  960 /  rows;
+
+  $('.grid-square').remove();
+
+  for ( ; x < ( rows * rows ) ; x++ ) {
+    $( '#overlord' ).append( "<div class='grid-square'></div>" );
+  }
+
+  $('.grid-square').css ( 'height', squareHeightWidth + 'px' );
+  $('.grid-square').css ( 'width', squareHeightWidth + 'px' );
+
+  mouseTrailMode();
+
+};
+
+function mouseTrailMode () {
 
   $( '.grid-square' )
     .mouseenter( function() {
@@ -13,20 +49,6 @@ var main = function() {
 
       $(this).addClass( 'active' );
   });
-
-};
-
-var drawGrid = function ( tiles ) {
-  x = 0;
-
-  var tilesPerRow = Math.sqrt( tiles );
-
-  for ( ; x < tiles ; x++ ) {
-    $( '#overlord' ).append( "<div class='grid-square'></div>" );
-  }
-
-  $('.grid-square').width ( (960 -  tilesPerRow * 2)  / tilesPerRow );
-  $('.grid-square').height ( (960 - tilesPerRow * 2) / tilesPerRow );
 
 };
 
